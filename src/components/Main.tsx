6f3679ab1f284e7353f74e01c19561309769f30a
Ivan Yurygin <sivan2140@gmail.com>
2023-12-21 09:10:58 +0300

    secondCommit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,7 @@
 import '../css/main.css'
-import RecipeList from './RecipeList'
 import { Outlet } from 'react-router-dom'
 
-interface MainProps {
-	children: React.ReactNode
-}
-
-const Main: React.FC = () => {
+const Main = () => {
 
 
 	return (
--- src/components/RecipePage.tsx
+++ src/components/RecipePage.tsx
@@ -1,7 +1,6 @@
 import '../css/recipePage.css'
 import { useParams } from 'react-router-dom'
 import { useGetRecipeQuery } from '../servises/recipeApi'
-import { useEffect } from 'react'
 
 
 const RecipePage = () => {
@@ -9,11 +8,25 @@ const RecipePage = () => {
 	const { id } = useParams()
 	const { data: recipe } = useGetRecipeQuery(String(id))
 
+
 	return (
 		<section className="recipePage">
-			{recipe && recipe.name} 
-			<br />
-			{recipe?.description}
+			<h1>{recipe && recipe.name}</h1> 
+			<p>{recipe?.description}</p>
+			<div>
+				<h1>Ингредиенты:</h1>
+				<ul>
+					{recipe && recipe.ingredients.map(ingredient => (
+						<ul key={ingredient.name}>{ingredient.name} - {ingredient.amount}.</ul>
+					))}
+				</ul>
+			</div>
+			<div>
+				<h1>Приготовление:</h1>
+				{recipe && recipe.steps.map(step => (
+					<p key={step.num}>{step.num}. {step.body}</p>
+				))}
+			</div>
 		</section>
 	)
 }
